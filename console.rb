dd32521e4674716dc582778c268373e6dc41d551
Kynan <k.song@blueyonder.co.uk>
2017-10-10 20:04:08 +0100

    Added def save() to bounties.rb

---
+++ console.rb
@@ -0,0 +1,32 @@
+require ( 'pry' )
+require_relative ( 'models/bounties.rb' )
+
+bounty_1 = Bounties.new(
+  {
+  'name' => 'Kurr',
+  'species' => 'Omicronian',
+  'homeworld' => 'Omicron Persei 8',
+  'bounty_value' => '2300'
+  }
+)
+
+bounty_2 = Bounties.new(
+  {
+  'name' => 'Mork',
+  'species' => 'Unknown',
+  'homeworld' => 'Unknown',
+  'bounty_value' => '50000'
+  }
+)
+
+bounty_3 = Bounties.new(
+  {
+  'name' => 'Nebula',
+  'species' => 'Luphomoid',
+  'homeworld' => 'Unknown',
+  'bounty_value' => '30000'
+  }
+)
+
+binding.pry
+nil
--- models/bounties.rb
+++ models/bounties.rb
@@ -4,12 +4,30 @@ class Bounties
   attr_accessor :name, :species, :homeworld, :bounty_value
   attr_reader :id
 
-  def initialize
+  def initialize(options)
     @id = options['id'].to_i if options['id']
     @name = options['name']
     @species = options['species']
     @homeworld = options['homeworld']
-    @bouty_value = options['bouty_value'].to_i
+    @bounty_value = options['bounty_value'].to_i
   end
 
+  def save()
+    db = PG.connect({
+      dbname: 'spacecowboys',
+      host: 'localhost'
+      })
+    sql = "
+      INSERT INTO bounty_info (name, species, homeworld, bounty_value)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *
+    "
+    values = [@name, @species, @homeworld, @bounty_value]
+    db.prepare("save", sql)
+
+    results = db.exec_prepared("save", values)
+
+    @id = results[0]['id'].to_i
+    db.close()
+  end
 end
